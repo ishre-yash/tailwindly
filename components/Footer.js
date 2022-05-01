e784f23502ecaf5b6ec6569c81d35d81ccc70b02
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-05-01 19:04:55 +0530

    NEXT JS

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (<>
+    <footer className="w-full max-w-container mx-auto border-t py-10 text-center text-sm text-gray-500 sm:flex sm:items-center sm:justify-center">
+        <p>© 2022 Tailwindly All rights reserved.</p>
+        <p className="mt-2 sm:mt-0 sm:ml-3 sm:border-l sm:border-gray-200 sm:pl-3">
+            <a className="hover:text-teal-600" href="/privacy-policy">Privacy Policy</a>
+        </p>
+    </footer>
+  </>
+  )
+}
+
+export default Footer
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
+import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return < > 
+  <Head>
+        <title>Tailwindly</title>
+        <meta name="description" content="Tailwindly is a powerful frontend library of tailwind components enhanced by vanilla js. It helps you build interfaces with advanced interactions and animations." />
+        <link rel="icon" type="image/png" sizes="512x512" href="/logo/android-chrome-512x512.png" />
+    <link rel="icon" type="image/png" sizes="192x192" href="/logo/android-chrome-192x192.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/logo/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/logo/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/logo/favicon-16x16.png" />
+    <link rel="shortcut icon" href="/logo/favicon.ico" type="image/x-icon" />
+    <link rel="manifest" href="/logo/site.webmanifest" />
+      </Head>
+  <ThemeProvider attribute="class">
+    
+  <NavBar/>
+  < Component {...pageProps}/> 
+  <Footer/>
+  </ThemeProvider>
+  </>
 }
 
-export default MyApp
+    export default MyApp
